06bc2834087270a5a35c4595b976869769c2e425
Sachin Chavan <sachinewx@gmail.com>
2018-03-15 14:54:29 +0530

    login logout implimentation

--- advert/urls.py
+++ advert/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^login/$', auth_view.login, {'template_name': 'advert/login.html'}, name='login'),
+    url(r'^logout/$', auth_view.logout, name='logout'),
     url(r'^(?P<slug>[\w-]+)/$', views.district_view, name='city_view'),
     url(r'(?P<pk>\d+)/(?P<slug>[\w-]+)/$', views.add_view, name='add_view'),
     url(r'(?P<title>[\w-]+)/(?P<pk>\d+)/$', views.advert),
